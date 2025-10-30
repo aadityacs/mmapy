83ad75b9dbcf817d870edeeac94b3af7764d175b
Aaditya Chandrasekhar <achandrasek3@wisc.edu>
2025-10-30 16:56:54 -0500

    add lagrange params return

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 """Setup mmapy."""
+
 import setuptools
 
 setuptools.setup(
-    name='mmapy',
-    version='0.0.1',
-    license='GPL-2.0',
-    author='Aaditya Chandrasekhar',
-    author_email='cs.aaditya@gmail.com',
-    install_requires=[
-        'dataclasses',
-        'absl',
-        'jax',
-        'numpy',
-        'scipy',
-        'absl-py',
-        'pytest',
-    ],
-    url='https://github.com/aadityacs/mmapy',
-    packages=setuptools.find_packages(),
-    python_requires='>=3',
+  name="mmapy",
+  version="0.0.2",
+  license="MIT",
+  author="Aaditya Chandrasekhar",
+  author_email="cs.aaditya@gmail.com",
+  install_requires=[
+    "dataclasses",
+    "absl",
+    "jax",
+    "numpy",
+    "scipy",
+    "absl-py",
+    "pytest",
+  ],
+  url="https://github.com/aadityacs/mmapy",
+  packages=setuptools.find_packages(),
+  python_requires=">=3",
 )
